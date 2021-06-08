0ed127725de0014555b7258b27da739499d52be6
lucast52501 <lt233@njit.edu>
2021-06-07 21:27:04 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,7 @@
-<?php echo "It works!";?>
+<h1> Lucas Terrone's Website </h1>
+
+<a href="https://lt233-dev.herokuapp.com/Problem1.php" name="Problem 1">Problem 1 </a>
+
+<a href="https://lt233-dev.herokuapp.com/Problem2.php" name="Problem 2">Problem 2 </a>
+
+<a href="https://lt233-dev.herokuapp.com/Problem3.php" name="Problem 3">Problem 3 </a>
