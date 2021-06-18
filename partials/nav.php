9914c343ea98596473229f93ce2a4db51737a889
lucast52501 <lt233@njit.edu>
2021-06-17 20:05:53 -0400

    Initial Milestone1 commit

---
+++ partials/nav.php
@@ -0,0 +1,62 @@
+<?php
+//Note: this is to resolve cookie issues with port numbers
+$domain = $_SERVER["HTTP_HOST"];
+if (strpos($domain, ":")) {
+    $domain = explode(":", $domain)[0];
+}
+session_set_cookie_params([
+    "lifetime" => 60 * 60,
+    "path" => "/Project",
+    //"domain" => $_SERVER["HTTP_HOST"] || "localhost",
+    "domain" => $domain,
+    "secure" => true,
+    "httponly" => true,
+    "samesite" => "lax"
+]);
+session_start();
+require_once(__DIR__ . "/../lib/functions.php");
+?>
+<style>
+    body {margin: 0;}
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        background-color: #292F33;
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+    nav ul li {
+        float: left;
+    }
+    nav ul li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+    }
+    nav ul li a:hover:not(.active) {
+        background-color: #111;
+    }
+    .active {
+        background-color: Crimson;
+    }
+</style>
+<head><br></head>
+<nav>
+    <ul>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="home.php">Home</a></li>
+        <?php endif; ?>
+        <?php if (!is_logged_in()) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()) : ?>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
+</nav>
