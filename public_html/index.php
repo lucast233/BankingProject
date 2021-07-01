3b77fa233c01ee46549fc64633dbd5c3364d7073
lucast52501 <84740404+lucast52501@users.noreply.github.com>
2021-07-01 00:32:53 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
 <h1> Lucas Terrone's Website </h1>
 <br>
-<a href="https://lt233-dev.herokuapp.com/Project/login.php" name="Login">Banking Simulation Login </a>
+<a href="https://lt233-production.herokuapp.com/Project/login.php" name="Login">Banking Simulation Login </a>
 <br>
