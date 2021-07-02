651107d708bc3b1ef90b2b67c22ececb29b4fbff
lucast52501 <84740404+lucast52501@users.noreply.github.com>
2021-07-01 23:01:52 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,3 @@
-<h1> Lucas Terrone's Website </h1>
-<br>
-<a href="https://lt233-dev.herokuapp.com/problem1.html" name="Problem 1 for week 3">Problem 1 </a>
-<br>
+<h1>Home for HTML5 Canvas Game</h1>
+<h3><a href="modGame.html">Modified Game</a></h3>
+<h3><a href="origGame.html">Original Game</a></h3>
