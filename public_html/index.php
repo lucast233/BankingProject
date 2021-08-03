f1c6e1aa741e3ad28021fc58ed2ea0fcd72a7330
lucast52501 <84740404+lucast52501@users.noreply.github.com>
2021-08-02 21:59:25 -0400

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -2,4 +2,4 @@
 <h3><a href="modGame.html">Modified Game</a></h3>
 <h3><a href="origGame.html">Original Game</a></h3>
 <h3><a href="Project/login.php">Bank Project</a></h3>
-&nbsp;<small>Milestone 3</small>
+&nbsp;<small>Milestone 4</small>
